5881153d04f4a4f8b1e875d5f4e8dc21e5026db4
Alexander Römer <alex@roemer.design>
2019-12-24 10:16:06 +0100

    Initial commit

--- ShakeAnimation/ContentView.swift
+++ ShakeAnimation/ContentView.swift
@@ -8,9 +8,35 @@
 
 import SwiftUI
 
+struct Shake: GeometryEffect {
+    var amount: CGFloat = 10
+    var shakesPerUnit   = 3
+    var animatableData  : CGFloat
+    
+    func effectValue(size: CGSize) -> ProjectionTransform {
+        ProjectionTransform(CGAffineTransform(translationX: amount * sin(animatableData * .pi * CGFloat(shakesPerUnit)), y: 0))
+    }
+}
+
+
 struct ContentView: View {
+    @State var attempts: Int = 0
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Spacer()
+            Rectangle()
+                .fill(Color.pink)
+                .frame(width: 200, height: 50)
+                .cornerRadius(10)
+                .modifier(Shake(animatableData: CGFloat(attempts)))
+            Button(action: {
+                withAnimation(.default) {
+                    self.attempts += 1
+                }
+            }, label: { Text("Login") })
+            Spacer()
+        }
     }
 }
 
